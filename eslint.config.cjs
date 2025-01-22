fafa782c791dc6a619a5cb1e9a6a738a6970a14a
Harry Ryan <108880766+harryryan1@users.noreply.github.com>
2025-01-22 09:43:04 +0000

    Fix deployment issues (#9)
    * Update package versions (#7)
    
    * Moved redocly to prod dependencies (#8)

---
+++ eslint.config.cjs
@@ -0,0 +1,59 @@
+'use strict';
+
+const prettier = require('eslint-plugin-prettier');
+const fp = require('eslint-plugin-fp');
+const globals = require('globals');
+const js = require('@eslint/js');
+const {FlatCompat} = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+module.exports = [
+    {
+        ignores: ['**/coverage', '**/stuff'],
+    },
+    ...compat.extends('airbnb-base', 'prettier', 'plugin:jest/recommended'),
+    {
+        plugins: {
+            prettier,
+            fp,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            ecmaVersion: 2021,
+            sourceType: 'commonjs',
+
+            parserOptions: {
+                ecmaFeatures: {
+                    modules: false,
+                    classes: false,
+                },
+            },
+        },
+
+        rules: {
+            'prettier/prettier': ['error'],
+            'linebreak-style': ['error', 'unix'],
+            curly: ['error', 'all'],
+            'jest/expect-expect': ['error'],
+            strict: ['error', 'safe'],
+            'import/extensions': ['off'],
+            'fp/no-class': 'error',
+
+            'no-param-reassign': [
+                'error',
+                {
+                    props: false,
+                },
+            ],
+        },
+    },
+];
